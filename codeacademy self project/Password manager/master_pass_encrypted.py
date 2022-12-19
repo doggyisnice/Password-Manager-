db13191a70aa29bf692a306e2c9ec47f378b71e2
doggyisnice <119186374+doggyisnice@users.noreply.github.com>
2022-12-19 18:26:04 +0800

    Update master_pass_encrypted.py

--- codeacademy self project/Password manager/master_pass_encrypted.py
+++ codeacademy self project/Password manager/master_pass_encrypted.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
-
-master_password = "693042"
+#input your master_password
+master_password = ""
 key = Fernet.generate_key()
 fernet = Fernet(key)
 enc_master_password = fernet.encrypt(master_password.encode()).decode()
